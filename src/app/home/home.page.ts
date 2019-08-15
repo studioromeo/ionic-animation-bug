812b9453c5681a29347f909d18e08948bb388c85
Robert Rhoades <robert.rhoades@boxuk.com>
2019-08-15 17:36:28 +0100

    Add transition animation bug example

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { of } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
+  private stuff$ = of(true).pipe(delay(1000));
 
   constructor() {}
 
+  onClick() {
+    console.log('clicked');
+  }
 }
